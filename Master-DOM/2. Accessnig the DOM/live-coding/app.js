9e168f5e64c84831ba5cfb986e5494214e31561c
hemanth-92 <hemanth9679@gmail.com>
2024-01-21 20:45:43 +0530

    updated

--- Master-DOM/2. Accessnig the DOM/live-coding/app.js
+++ Master-DOM/2. Accessnig the DOM/live-coding/app.js
@@ -1,9 +1,32 @@
 // ************ DOM SELECTORS ************
 // 1. getElementsByTagName
+
+console.log(document.getElementsByTagName("h1"));
+console.log(document.getElementsByTagName("h1").length);
+
 // 2. getElementById
+
+console.log(document.getElementById("main"));
+
 // 3. getElementsByClassName
+
+console.log(document.getElementsByClassName("cls"));
+console.log(document.getElementsByClassName("cls-1"));
+
 // 4. querySelector
+// it select only one query
+
+console.log(document.querySelector("h1"));
+console.log(document.querySelector(".cls")); //  . is used for the class in query selector
+console.log(document.querySelector("#main")); //  #(Hash) is used for the id  in query selector
+console.log(document.querySelector("#id-1")); //  #(Hash) is used for the id  in query selector
 // 5. querySelectorAll
+
+console.log(document.querySelectorAll(".cls")); //it select all  class with cls
+console.log(document.querySelectorAll("li")); //it select all  class with cls
 
 // ---------------------------------------
 // Storing data in variables
+
+const h1 = document.getElementsByTagName("h1");
+console.log(h1);
