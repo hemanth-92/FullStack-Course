42a08de2c6eadd23ee963e607f04724f420b0a80
hemanth-92 <hemanth9679@gmail.com>
2024-01-21 22:20:29 +0530

    updated

--- Master-DOM/3. innerText, textContent, innerHTML/live coding/app.js
+++ Master-DOM/3. innerText, textContent, innerHTML/live coding/app.js
@@ -1,3 +1,15 @@
 // innerText
 // textContent
 // innerHTML
+
+const p = document.querySelector("p");
+console.log(p.innerText); //gives pure text
+
+console.log(p.textContent); //gives content inside the p tag
+
+console.log(p.innerHTML); //gives content inside the p tag along with different tags
+
+const h1 = document.querySelector("h1");
+h1.innerText = "text changed";
+
+h1.innerHTML = "<del>hemanth</del>";
