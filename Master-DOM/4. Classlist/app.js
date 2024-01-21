d6049448e4f797f8219ec451743c39fc70262ad1
hemanth-92 <hemanth9679@gmail.com>
2024-01-21 22:43:55 +0530

    updated

--- Master-DOM/4. Classlist/app.js
+++ Master-DOM/4. Classlist/app.js
@@ -1,4 +1,18 @@
 // classlist
+
+const h1 = document.querySelector("h1");
+console.log(h1.classList);
 // add()
+
+h1.classList.add("style"); // add class to elements
+
 // remove()
+
+h1.classList.remove("style"); // remove class to elements
+
 // toggle()
+
+// h1.classList.toggle("apple");
+h1.classList.toggle("styles");
+
+// if we have class it remove class and viscera
