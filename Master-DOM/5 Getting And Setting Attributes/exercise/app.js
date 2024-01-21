d5264005f5d5af3cc069b37c75e893f00100ba38
hemanth-92 <hemanth9679@gmail.com>
2024-01-21 23:15:37 +0530

    updated

--- Master-DOM/5 Getting And Setting Attributes/exercise/app.js
+++ Master-DOM/5 Getting And Setting Attributes/exercise/app.js
@@ -1,4 +1,13 @@
 // 1. Select the anchor tag
+const a = document.querySelector("a");
+console.log(a);
 // 2. Use getAttribute(attrName) to check the attribute.
+
+const href = document.querySelector("href");
+
 // 3. Select a-two & set "href" attribute
 // setAttribute(attrName, value)
+
+const a2 = document.querySelector("a-two");
+a2.href("href", "https://www.instagram.com/");
+console.log(a2);
--- Master-DOM/5 Getting And Setting Attributes/live coding/app.js
+++ Master-DOM/5 Getting And Setting Attributes/live coding/app.js
@@ -1,5 +1,26 @@
 // href
+
+const a = document.querySelector("a");
+const input = document.querySelector("input");
+console.log(a.href);
+
 // value
+
+console.log(input.value); // empty bcz no value and after entering value we get it
+
 // type
+
+console.log(input.type);
+
+console.log((a.href = "https://www.instagram.com/"));
+
+console.log((input.type = "password"));
+console.log((input.placeholder = " set strong password "));
 // getAttribute(attrName)
+console.log(input.getAttribute("type"));
+console.log(input.getAttribute("placeholder")); //get the attribute
+
 // setAttribute(attrName, value)
+
+console.log(input.setAttribute("placeholder", "password"));
+console.log(input.setAttribute("type", "password"));
