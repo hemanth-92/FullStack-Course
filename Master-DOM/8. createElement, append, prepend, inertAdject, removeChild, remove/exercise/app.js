2fa22a0ed938191275133f818fb47e6fb78fe6c5
hemanth-92 <hemanth9679@gmail.com>
2024-01-22 12:00:09 +0530

    updated

--- Master-DOM/8. createElement, append, prepend, inertAdject, removeChild, remove/exercise/app.js
+++ Master-DOM/8. createElement, append, prepend, inertAdject, removeChild, remove/exercise/app.js
@@ -1,5 +1,25 @@
 // createElement()
+
+const h1 = document.createElement("h1");
+const body = document.body;
+body.appendChild(h1);
+
+h1.innerText = "create h1 element ";
+h1.style.color = "red";
+
 // append()
+
+const a = document.createElement("a");
+body.appendChild(a);
+a.innerText = "https://twitter.com/?";
+a.style.color = "orange";
+
+const h4 = document.createElement("h4");
+h4.innerText = "twitter link";
+h4.style.color = "crimson";
+
+a.append(h4);
+a.prepend(h4);
 // prepend()
 
 /* insertAdjacentElement(
@@ -10,5 +30,21 @@
     element
 ) */
 
+const firstP = document.querySelector("p");
+const ip = document.createElement("ip");
+ip.innerText = "I am learning  DOM";
+ip.style.color = "red";
+firstP.insertAdjacentElement("beforebegin", ip);
+firstP.insertAdjacentElement("beforeend", ip);
+firstP.insertAdjacentElement("afterbegin", ip);
+firstP.insertAdjacentElement("afterend", ip);
+
 // removeChild()
+
+const ul = document.querySelector("ul");
+const second = document.querySelector(".second");
+ul.removeChild(second);
+
 // remove()
+
+ul.remove();
--- Master-DOM/8. createElement, append, prepend, inertAdject, removeChild, remove/live coding/app.js
+++ Master-DOM/8. createElement, append, prepend, inertAdject, removeChild, remove/live coding/app.js
@@ -1,9 +1,38 @@
 // ------------------------------------
 // createElement()
+
+const h1 = document.createElement("h1");
+h1.textContent = "hello world";
+h1.classList = "greetings";
+
 // appendChild()
+
+const body = document.body;
+body.appendChild(h1);
 // append()
+
+const section = document.querySelector("section");
+const i = document.createElement("i");
+i.innerText = "I am learning java script";
+i.style.color = "teal";
+
+const h4 = document.createElement("h4");
+h4.innerText = "hemanth";
+h4.style.color = "crimson";
+
+section.append(i, h4);
+section.prepend(i, h4);
+
 // prepend()
-// insertBefore()
+// selector.insertBefore(what,where)
+
+const ul = document.querySelector("ul");
+const newLi = document.createElement("li");
+newLi.innerText = " li tag";
+ul.appendChild(newLi);
+
+const firstLi = document.querySelector("li");
+ul.insertBefore(newLi, firstLi);
 
 /* insertAdjacentElement(
     'beforebegin'
@@ -13,6 +42,23 @@
     element
 ) */
 
+const firstP = document.querySelector("p");
+const ip = document.createElement("ip");
+ip.innerText = "I am learning  DOM";
+ip.style.color = "skyblue";
+firstP.insertAdjacentElement("beforebegin", ip);
+firstP.insertAdjacentElement("beforeend", ip);
+firstP.insertAdjacentElement("afterbegin", ip);
+firstP.insertAdjacentElement("afterend", ip);
+
 // removeChild()
+
+const newList = document.querySelector(".new-list");
+const fourth = document.querySelector(".fourth");
+newList.removeChild(fourth);
+
 // remove()
+
+newList.remove();
+
 // ------------------------------------
