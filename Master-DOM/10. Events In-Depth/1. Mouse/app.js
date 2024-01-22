39d8c6e16525d5d80591b35eacdcc06732b6e5bd
hemanth-92 <hemanth9679@gmail.com>
2024-01-22 13:25:48 +0530

    updated

--- Master-DOM/10. Events In-Depth/1. Mouse/app.js
+++ Master-DOM/10. Events In-Depth/1. Mouse/app.js
@@ -1,23 +1,61 @@
 // https://www.w3schools.com/jsref/obj_mouseevent.asp
 
 // 1. click
-// 2. contextmenu
-// 3. dblclick
-// 4. mousedown
-// 5. mouseenter
-// 6. mouseleave
-// 7. mousemove
-// 8. mouseout
-// 9. mouseover
-// 10. mouseup
-
 let one = document.querySelector(".click");
+one.addEventListener("click", () => {
+  one.classList.toggle("red");
+});
+
+// 2. contextmenu
 let two = document.querySelector(".contextmenu");
+two.addEventListener("contextmenu", () => {
+  two.classList.toggle("blue");
+});
+
+// 3. dblclick
 let three = document.querySelector(".dblclick");
+three.addEventListener("dblclick", () => {
+  three.classList.toggle("teal");
+});
+
+// 4. mousedown
 let four = document.querySelector(".mousedown");
+four.addEventListener("mousedown", () => {
+  four.classList.toggle("steelblue");
+});
+
+// 5. mouseenter
 let five = document.querySelector(".mouseenter");
+five.addEventListener("mouseenter", () => {
+  five.classList.toggle("orange");
+});
+
+// 6. mouseleave
 let six = document.querySelector(".mouseleave");
+six.addEventListener("mouseleave", () => {
+  six.classList.toggle("purple");
+});
+
+// 7. mousemove
 let seven = document.querySelector(".mousemove");
+seven.addEventListener("mousemove", () => {
+  seven.classList.toggle("pink");
+});
+
+// 8. mouseout
 let eight = document.querySelector(".mouseout");
+eight.addEventListener("mouseout", () => {
+  eight, classList.toggle;
+});
+
+// 9. mouseover
 let nine = document.querySelector(".mouseover");
+nine.addEventListener("mouseover", () => {
+  nine.classList.toggle("chocolate");
+});
+
+// 10. mouseup
 let ten = document.querySelector(".mouseup");
+ten.addEventListener("mouseup", () => {
+  ten.classList.toggle("aqua");
+});
--- Master-DOM/10. Events In-Depth/2. Keyboard/app.js
+++ Master-DOM/10. Events In-Depth/2. Keyboard/app.js
@@ -1,2 +1,32 @@
 // https://www.w3schools.com/jsref/obj_keyboardevent.asp
+
 const input = document.querySelector("input");
+input.addEventListener("keypress", () => {
+  console.log("key pressed");
+});
+
+input.addEventListener("keyup", () => {
+  console.log("keyup");
+});
+
+input.addEventListener("keydown", () => {
+  console.log("keydown");
+});
+
+// KeyboardEvent Properties
+
+input.addEventListener("keypress", (e) => {
+  console.log(e.charCode);
+});
+
+input.addEventListener("keypress", (e) => {
+  console.log(e.code);
+});
+
+input.addEventListener("keypress", (e) => {
+  console.log(e.key);
+});
+
+input.addEventListener("keypress", (e) => {
+  console.log(e.shiftKey);
+});
