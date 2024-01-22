fa4cc9b22b20113526d78bd7f84c86aa0a3c9fbe
hemanth-92 <hemanth9679@gmail.com>
2024-01-22 12:47:19 +0530

    updated

--- Master-DOM/9. 3 ways to add events/app.js
+++ Master-DOM/9. 3 ways to add events/app.js
@@ -1,5 +1,30 @@
 // ----------- BAD WAY-----------
 
+const secondBtn = document.querySelector(".second-btn");
+secondBtn.onclick = function () {
+  console.log("hemanth");
+};
+
 // ----------- GREAT WAY-----------
 
+const best = document.querySelector(".best");
+function greet() {
+  console.log("hello world");
+}
+best.addEventListener("click", greet);
+
 // ----------- Event (e) Object -----------
+
+const para = document.querySelector(".para");
+para.addEventListener("click", (event) => console.log(event));
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log(event);
+});
+input.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(input.value);
+});
