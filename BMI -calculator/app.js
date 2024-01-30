d5085b8fd059ae87faab9eb08b60ca58169e7853
hemanth-92 <hemanth9679@gmail.com>
2024-01-30 18:07:45 +0530

    BMI calculator

--- BMI -calculator/app.js
+++ BMI -calculator/app.js
@@ -2,6 +2,7 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  let bmi;
   const height = parseInt(document.querySelector("#height").value);
   const weight = parseInt(document.querySelector("#weight").value);
   const results = document.querySelector("#results");
@@ -10,15 +11,15 @@ form.addEventListener("submit", (e) => {
   } else if (weight === "" || weight < 0 || isNaN(weight)) {
     results.innerHTML = "Please give vaild weight";
   } else {
-    const bmi = weight / ((height * height) / 10000).toFixed(2);
+    bmi = weight / ((height * height) / 10000).toFixed(2);
     results.innerHTML = `<span>${bmi}</span>`;
   }
-  const guide = document.querySelector(".guide-result");
+  const guide = document.querySelector("#guide");
   if (bmi < 18.6) {
-    guide.innerHTML = `<span>${underweight}</span>`;
+    guide.innerHTML = "Your BMI index is Underweight";
   } else if (bmi > 18.6 && bmi < 24.9) {
-    guide.innerHTML = `<span>${normal}</span>`;
+    guide.innerHTML = "Your BMI index is Normal";
   } else {
-    guide.innerHTML = `<span>${overweight}</span>`;
+    guide.innerHTML = "Your BMI index is Overweight";
   }
 });
